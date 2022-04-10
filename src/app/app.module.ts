a222818d34b97ac52d5d50f8664e2acafa0cc761
Liz <adhiamboliz3@gmail.com>
2022-04-10 10:43:15 +0300

    Added quotes component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { QuotesComponent } from './quotes/quotes.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavBarComponent
+    NavBarComponent,
+    QuotesComponent
   ],
   imports: [
     BrowserModule,
