af8d87d900b913c3b573020316433682ac3bcd8d
Liz <adhiamboliz3@gmail.com>
2022-04-10 11:04:44 +0300

    Generated quotes-app component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
 import { QuotesComponent } from './quotes/quotes.component';
+import { QuotesFormComponent } from './quotes-form/quotes-form.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavBarComponent,
-    QuotesComponent
+    QuotesComponent,
+    QuotesFormComponent
   ],
   imports: [
     BrowserModule,
